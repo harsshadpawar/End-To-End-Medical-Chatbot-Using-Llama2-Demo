3ce97b30bef6da80074b670441ef51e720ab9350
Harsshad-pawar <pawar.harshad186@gmail.com>
2024-02-18 22:07:14 +0400

    Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.1',
+    author= 'Harsshad Pawar',
+    author_email= 'pawar.harshad186@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
